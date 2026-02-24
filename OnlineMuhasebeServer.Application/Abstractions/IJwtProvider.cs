7fd59dadaae03030518b38ee830a008c166eb6c9
Adem Çok <ademcok0693@gmail.com>
2026-02-24 18:24:16 +0300

    Json Web Token, Login, Options Pattern

---
+++ OnlineMuhasebeServer.Application/Abstractions/IJwtProvider.cs
@@ -0,0 +1,9 @@
+﻿using OnlineMuhasebeServer.Domain.AppEntites.Identity;
+
+namespace OnlineMuhasebeServer.Application.Abstractions
+{
+    public interface IJwtProvider
+    {
+        Task<string> CreateTokenAsync(AppUser user, IList<string> roles);
+    }
+}
--- OnlineMuhasebeServer.WebApi/Configurations/ApplicationServiceInstaller.cs
+++ OnlineMuhasebeServer.WebApi/Configurations/ApplicationServiceInstaller.cs
@@ -1,4 +1,5 @@
 ﻿
+using MediatR;
 using OnlineMuhasebeServer.Application;
 
 namespace OnlineMuhasebeServer.WebApi.Configurations
