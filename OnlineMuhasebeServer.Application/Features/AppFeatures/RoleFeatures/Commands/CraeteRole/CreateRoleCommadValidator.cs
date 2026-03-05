83c415e8485c5d4a0bd4d032a563c606f015a550
Adem Çok <ademcok0693@gmail.com>
2026-03-05 23:58:49 +0300

    Validators kodları yazıldı

---
+++ OnlineMuhasebeServer.Application/Features/AppFeatures/RoleFeatures/Commands/CraeteRole/CreateRoleCommadValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace OnlineMuhasebeServer.Application.Features.AppFeatures.RoleFeatures.Commands.CraeteRole;
+
+public sealed class CreateRoleCommadValidator : AbstractValidator<CreateRoleCommand>
+{
+    public CreateRoleCommadValidator()
+    {
+        RuleFor(p => p.Code).NotEmpty().WithMessage("Role kodu boş olamaz!");
+        RuleFor(p => p.Code).NotNull().WithMessage("Role kodu boş olamaz!");
+        RuleFor(p => p.Name).NotEmpty().WithMessage("Role adı boş olamaz!");
+        RuleFor(p => p.Name).NotNull().WithMessage("Role adı boş olamaz!");
+    }
+}
